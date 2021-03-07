765cf543cccf6119ff2206c861df85d0ae9adb67
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-03-07 16:36:38 +0100

    Add table billing

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,14 +3,13 @@ CREATE DATABASE puddy;
 USE puddy;
 
 
-
 CREATE TABLE user (
                 id INT AUTO_INCREMENT NOT NULL,
                 first_name VARCHAR(20) NOT NULL,
                 last_name VARCHAR(20) NOT NULL,
                 password VARCHAR(20) NOT NULL,
                 email VARCHAR(50) NOT NULL,
-                balance DOUBLE DEFAULT 0 NOT NULL,
+                balance DOUBLE PRECISION NOT NULL,
                 currency VARCHAR(3) NOT NULL,
                 PRIMARY KEY (id)
 );
@@ -29,7 +28,7 @@ CREATE TABLE versement (
                 bank_account_iban VARCHAR(50) NOT NULL,
                 date DATETIME NOT NULL,
                 description VARCHAR(100),
-                amount DOUBLE NOT NULL,
+                amount DOUBLE PRECISION NOT NULL,
                 currency VARCHAR(3) NOT NULL,
                 PRIMARY KEY (id)
 );
@@ -37,13 +36,23 @@ CREATE TABLE versement (
 
 CREATE TABLE transfer (
                 id INT AUTO_INCREMENT NOT NULL,
-                transmitter_user_id INT NOT NULL,
                 recipient_user_id INT NOT NULL,
+                source_user_id INT NOT NULL,
                 date DATETIME NOT NULL,
-                amount DOUBLE NOT NULL,
+                amount DOUBLE PRECISION NOT NULL,
                 currency VARCHAR(3) NOT NULL,
-                tax FLOAT NOT NULL,
+                tax DOUBLE PRECISION NOT NULL,
                 description VARCHAR(100) NOT NULL,
+                PRIMARY KEY (id)
+);
+
+
+CREATE TABLE billing (
+                id INT AUTO_INCREMENT NOT NULL,
+                transfer_id INT NOT NULL,
+                user_id INT NOT NULL,
+                amount DOUBLE PRECISION NOT NULL,
+                status VARCHAR(5) NOT NULL,
                 PRIMARY KEY (id)
 );
 
@@ -69,7 +78,7 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
 ALTER TABLE transfer ADD CONSTRAINT user_transaction_fk
-FOREIGN KEY (transmitter_user_id)
+FOREIGN KEY (source_user_id)
 REFERENCES user (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
@@ -86,9 +95,21 @@ REFERENCES user (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
+ALTER TABLE billing ADD CONSTRAINT user_billing_fk
+FOREIGN KEY (user_id)
+REFERENCES user (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
 ALTER TABLE versement ADD CONSTRAINT bankaccount_banktransfer_fk
 FOREIGN KEY (bank_account_iban)
 REFERENCES bank_account (iban)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
+
+ALTER TABLE billing ADD CONSTRAINT transfer_billing_fk
+FOREIGN KEY (transfer_id)
+REFERENCES transfer (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
