e8e88828ebfc5b051b253d3b7185f70099427540
Mathias Lauer <mathias.lauer.fr@gmail.com>
2021-03-16 13:05:22 +0100

    Add register success template, add login failure message

--- src/main/java/com/paymybuddy/puddy/controller/MainController.java
+++ src/main/java/com/paymybuddy/puddy/controller/MainController.java
@@ -12,11 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.paymybuddy.puddy.enums.CURRENCY;
@@ -45,7 +42,11 @@ public class MainController {
 	}
 	
 	@GetMapping(value="/login")
-	public String login() {
+	public String login(@RequestParam(required = false) Object error, Model model) {
+		if(error != null) {
+			log.warn("User failed to login");
+			model.addAttribute("error", "error");
+		}
 		return "login";
 	}
 	
@@ -93,9 +94,9 @@ public class MainController {
 	@PostMapping(value="/register")
 	public String submitRegister(@Valid UserForm userForm, BindingResult bindingResult, Model model) {
 		
-		
-		if(bindingResult.hasErrors()) {
 			model.addAttribute("userForm", userForm);
+		if(bindingResult.hasErrors()) {
+			
 			return "register";
 		}
 		else
